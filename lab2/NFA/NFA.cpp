4b2249f36c74fa2988c1457bea01ee63a37c2f0f
kargamant <firexholms@gmail.com>
2024-04-07 19:20:13 +0300

    NFA inner logic is done. To be tested.

--- lab2/NFA/NFA.cpp
+++ lab2/NFA/NFA.cpp
@@ -1,4 +1,5 @@
 #include <NFA.h>
+#include <string>
 
 namespace Nfa
 {
@@ -22,17 +23,86 @@ namespace Nfa
         stateMap[from].erase(to);
     }
 
-    NFA NFA::plusNFA(NFA& NFA1)
+
+    NFA::NFA(NFA& NFA1) : current(NFA1.current), id(NFA1.id), stateMap(NFA1.stateMap)
     {
-        NFA1.delete_state("start");
-        NFA1.delete_state("S0");
-        NFA1.delete_state("S1");
+        //stateMap.merge(NFA1.stateMap);
+    }
 
-        add_state("start");
-        add_state("S0");
-        add_state("S1");
+    NFA::NFA(NFA&& NFA1) :  stateMap(NFA1.stateMap), current(NFA1.current), id(NFA1.id)
+    {
+        NFA1.current="";
+        NFA1.stateMap.clear();
+    }
 
-        add_transition("start", "S0", "");
-        //add_transition();
+    NFA::NFA(int id, const std::string& transition) : id(id), current("start_"+std::to_string(id))
+    {
+        add_state("start_"+std::to_string(id));
+        add_state("end_"+std::to_string(id));
+        add_transition("start_"+std::to_string(id), "end_"+std::to_string(id), transition);
+    }
+
+    NFA& NFA::operator<<(NFA& nfa)
+    {
+        if(this!=&nfa && nfa.id!=id)
+        {
+            stateMap.merge(nfa.stateMap);
+        }
+        return *this;
+    }
+
+
+    NFA plusNFA(NFA& NFA1)
+    {
+        NFA nfa{NFA1};
+        //considering every NFA have its start and end
+        nfa.add_transition(NFA1.getEnd(), NFA1.getStart(), "");
+
+        return nfa;
+    }
+
+    NFA optNFA(NFA& NFA1)
+    {
+        NFA nfa{NFA1};
+
+        nfa.add_transition(NFA1.getStart(), NFA1.getEnd(), "");
+        return nfa;
+    }
+
+    NFA anyNFA(int id)
+    {
+        return NFA(id, ".");
+    }
+
+    NFA catNFA(NFA& NFA1, NFA& NFA2)
+    {
+        NFA nfa{NFA1};
+        nfa<<NFA2;
+        nfa.id=NFA1.id+NFA2.id;
+        nfa.add_state(nfa.getStart());
+        nfa.add_state(nfa.getEnd());
+
+        nfa.add_transition(nfa.getStart(), NFA1.getStart(), "");
+        nfa.add_transition(NFA2.getEnd(), nfa.getEnd(), "");
+
+        nfa.add_transition(NFA1.getEnd(), NFA2.getStart(), "");
+
+        return nfa;
+    }
+
+    NFA orNFA(NFA& NFA1, NFA& NFA2)
+    {
+        NFA nfa{NFA1};
+        nfa<<NFA2;
+        nfa.id=NFA1.id+NFA2.id;
+        nfa.add_state(nfa.getStart());
+        nfa.add_state(nfa.getEnd());
+
+        nfa.add_transition(nfa.getStart(), NFA1.getStart(), "");
+        nfa.add_transition(nfa.getStart(), NFA2.getStart(), "");
+
+        nfa.add_transition(NFA1.getEnd(), nfa.getEnd(), "");
+        nfa.add_transition(NFA2.getEnd(), nfa.getEnd(), "");
+        return nfa;
     }
 }
--- lab2/NFA/NFA.h
+++ lab2/NFA/NFA.h
@@ -1,6 +1,7 @@
 #ifndef AUTOMAT_H
 #define AUTOMAT_H
 #include <unordered_map>
+#include <string>
 
 namespace Nfa
 {
@@ -10,22 +11,37 @@ namespace Nfa
         //structure of table
         //from | <to, how>
         std::unordered_map<std::string, std::unordered_map<std::string, std::string>> stateMap;
-        char current;
+        std::string current;
+        int id;
 
-        NFA orNFA(NFA& NFA1, NFA& NFA2);
-        NFA catNFA(NFA& NFA1, NFA& NFA2);
-        NFA plusNFA(NFA& NFA1);
-        NFA optNFA(NFA& NFA1);
-        NFA anyNFA(NFA& NFA1);
         void add_state(const std::string& name);
         void add_transition(const std::string& from, const std::string& to, const std::string& condition);
         void delete_state(const std::string& name);
         void delete_transition(const std::string& from, const std::string& to);
     public:
+        NFA();
+        NFA(int id, const std::string& transition);
+        NFA(NFA& NFA1);
+        NFA(NFA&& NFA1);
         NFA(NFA& NFA1, NFA& NFA2, char op);
         NFA(NFA& NFA1, char op);
 
+        std::string getStart() {return "start_"+std::to_string(id);}
+        std::string getEnd() {return "end_"+std::to_string(id);}
+
+        NFA& operator<<(NFA& nfa);
+
+        friend NFA orNFA(NFA& NFA1, NFA& NFA2);
+        friend NFA catNFA(NFA& NFA1, NFA& NFA2);
+        friend NFA plusNFA(NFA& NFA1);
+        friend NFA optNFA(NFA& NFA1);
+        friend NFA anyNFA(int id);
     };
+    NFA orNFA(NFA& NFA1, NFA& NFA2);
+    NFA catNFA(NFA& NFA1, NFA& NFA2);
+    NFA plusNFA(NFA& NFA1);
+    NFA optNFA(NFA& NFA1);
+    NFA anyNFA(int id);
 }
 
 #endif // AUTOMAT_H
